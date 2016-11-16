f98af4943562d68323f3edac86119834a9676bc2
yonga <jorgeggayon@gmail.com>
2016-11-15 21:58:58 -0500

    First associations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,6 @@
 class Flight < ApplicationRecord
+  belongs_to :origin, class_name: "Airport", foreign_key: "origin_airport_id", optional: true
+  belongs_to :destination, class_name: "Airport", foreign_key: "destination_airport_id", optional: true
+  belongs_to :airline, class_name: "Airline", foreign_key: "airline_id", optional: true
+  has_many :flight_itineraries, class_name: "FlightItinerary"
 end
